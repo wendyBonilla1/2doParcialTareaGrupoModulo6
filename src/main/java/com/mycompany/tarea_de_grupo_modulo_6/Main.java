6e8cd9727f45381c472ca867f2f6031afa893434
Wendy Bonilla <wb45362@gmail.com>
2021-07-16 23:13:45 -0600

    feat: instantiate classes finish

--- src/main/java/com/mycompany/tarea_de_grupo_modulo_6/Main.java
+++ src/main/java/com/mycompany/tarea_de_grupo_modulo_6/Main.java
@@ -3,6 +3,10 @@ package com.mycompany.tarea_de_grupo_modulo_6;
 public class Main {
 
     public static void main(String[] args) {
-     
+        Persona deportista = new Deportista("Wendy", "Bonilla", 21, false, "0000-0000-0000");
+        System.out.println(" " + deportista.getnombre() + " " + deportista.getapellidos() + " " + deportista.getnumeroDocumentoidentidad() + " " + (deportista.getcasado()?"Casado(a)":"Soltero(a)") + " " + deportista.getedad());
+        System.out.println("");
+        Persona doctor = new Doctor("Juan", "Madrid", 30, true, "1111-1111-1111");
+        System.out.println(" " + doctor.getnombre() + " " + doctor.getapellidos() + " " + doctor.getnumeroDocumentoidentidad() + " " + (doctor.getcasado()?"Casado(a)":"Soltero(a)") + " " + doctor.getedad());
     }
 }
